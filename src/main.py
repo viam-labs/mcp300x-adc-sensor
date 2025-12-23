dc4a080aa1a1fa03da3818b2b8dae21b5d05a255
Grant Mulitz <gamulitz@gmail.com>
2025-12-23 13:44:18 +0100

    fix module so it works again

--- src/main.py
+++ src/main.py
@@ -1,24 +1,8 @@
 import asyncio
-
-from viam.components.sensor import Sensor
-from viam.resource.registry import Registry, ResourceCreatorRegistration
 from viam.module.module import Module
 from mcp300x import mcp3xxx
 
 
-async def main():
-    """This function creates and starts a new module, after adding all desired resources.
-    Resources must be pre-registered. For an example, see the `__init__.py` file.
-    """
-    Registry.register_resource_creator(
-        Sensor.SUBTYPE,
-        mcp3xxx.MODEL,
-        ResourceCreatorRegistration(mcp3xxx.new, mcp3xxx.validate),
-    )
-    module = Module.from_args()
-    module.add_model_from_registry(Sensor.SUBTYPE, mcp3xxx.MODEL)
-    await module.start()
-
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(Module.run_from_registry())
